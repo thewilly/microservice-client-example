0ad171cbeac1b6691e77f7c14cdfc346a7d39445
Guillermo Facundo Colunga <colunga91@gmail.com>
2018-04-19 21:20:04 +0200

    removed unused package
    Signed-off-by: Guillermo Facundo Colunga <colunga91@gmail.com>

--- src/main/java/io/github/thewilly/agents_client_example/clients/AgentsClient.java
+++
@@ -1,8 +0,0 @@
-package io.github.thewilly.agents_client_example.clients;
-
-import org.springframework.cloud.netflix.feign.FeignClient;
-
-@FeignClient("AgentsClient")
-public interface AgentsClient {
-
-}
